18f20b803e114cda7790bd18147eb9cae100f885
Ivar Stangeby <istangeby@gmail.com>
2023-02-12 15:01:29 +0100

    Add catpuccin oh-my-posh

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+oh-my-posh init fish --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/catppuccin.omp.json | source
